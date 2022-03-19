35c5cfa4075edb42b0533efba9ac3aaa80fb82a2
Asinoumiga <13189084050@163.com>
2022-03-19 23:12:42 +0800

    add 03/19 LeetCode file

---
+++ src/com/tello/tooffer/Offer10I.java
@@ -0,0 +1,29 @@
+package com.tello.tooffer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//剑指 Offer 10- I. 斐波那契数列
+public class Offer10I {
+    public int fib(int n) {
+        if(n==0)return 0;
+        if(n==1)return 1;
+        List<Integer> fibList = new ArrayList<>();
+        fibList.add(0);
+        fibList.add(1);
+        while (fibList.size()-1<n){
+            fibList.add((fibList.get(fibList.size()-2)+fibList.get(fibList.size()-1))%1000000007);
+        }
+        return fibList.get(n);
+    }
+
+    public int fib2(int n) {
+        if(n==1){
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+        return (fib2(n-1)+fib2(n-2)%1000000007);
+    }
+}
